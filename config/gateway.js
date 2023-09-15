35b1e4d414b2a14d4e7bca3f03361de2b46ac256
Nneka <59463614+BoboCalypso@users.noreply.github.com>
2023-09-15 13:15:17 -0500

    Update gateway.js

--- config/gateway.js
+++ config/gateway.js
@@ -4,7 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // <--- SET UP GATEWAY CONFIGURATION BELOW --->
-var gateway =  "Enter API credentials here"
+var gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "rb2zdhvv9n4r4bsc",
+  publicKey: "bfqym4rbfbnnk3bz",
+  privateKey: "b89c49b63d0cf136a972d65d9e745e90"
+});
 // <--- / END --->
 
 export default gateway;
